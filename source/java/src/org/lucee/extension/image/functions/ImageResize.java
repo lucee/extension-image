a8470965646beb6aaae8d15a133bdbf7e6eef16e
Michael Offner <michael@lucee.org>
2021-11-19 17:13:27 +0100

    fix blurfactor check

--- source/java/src/org/lucee/extension/image/functions/ImageResize.java
+++ source/java/src/org/lucee/extension/image/functions/ImageResize.java
@@ -27,48 +27,48 @@ import lucee.runtime.ext.function.Function;
 
 public class ImageResize extends FunctionSupport implements Function {
 
-    public static String call(PageContext pc, Object name) throws PageException {
-	return call(pc, name, null, null, "automatic", 1.0);
-    }
+	public static String call(PageContext pc, Object name) throws PageException {
+		return call(pc, name, null, null, "automatic", 1.0);
+	}
 
-    public static String call(PageContext pc, Object name, String width) throws PageException {
-	return call(pc, name, width, null, "automatic", 1.0);
-    }
+	public static String call(PageContext pc, Object name, String width) throws PageException {
+		return call(pc, name, width, null, "automatic", 1.0);
+	}
 
-    public static String call(PageContext pc, Object name, String width, String height) throws PageException {
-	return call(pc, name, width, height, "automatic", 1.0);
-    }
+	public static String call(PageContext pc, Object name, String width, String height) throws PageException {
+		return call(pc, name, width, height, "automatic", 1.0);
+	}
 
-    public static String call(PageContext pc, Object name, String width, String height, String interpolation) throws PageException {
-	return call(pc, name, width, height, interpolation, 1.0);
-    }
+	public static String call(PageContext pc, Object name, String width, String height, String interpolation) throws PageException {
+		return call(pc, name, width, height, interpolation, 1.0);
+	}
 
-    public static String call(PageContext pc, Object name, String width, String height, String interpolation, double blurFactor) throws PageException {
-	// image
-	// if(name instanceof String)name=pc.getVariable(Caster.toString(name));
-	Image image = Image.toImage(pc, name);
-	image = Image.createImage(pc, name, false, false, true, null);
-	interpolation = interpolation.toLowerCase().trim();
+	public static String call(PageContext pc, Object name, String width, String height, String interpolation, double blurFactor) throws PageException {
+		// image
+		// if(name instanceof String)name=pc.getVariable(Caster.toString(name));
+		Image image = Image.toImage(pc, name);
+		image = Image.createImage(pc, name, false, false, true, null);
+		interpolation = interpolation.toLowerCase().trim();
 
-	if (blurFactor <= 0.0 || blurFactor > 10.0) throw CFMLEngineFactory.getInstance().getExceptionUtil().createFunctionException(pc, "ImageResize", 5, "blurFactor",
-		"argument blurFactor must be between 0 and 10", null);
+		if (blurFactor < 0D || blurFactor > 10D) throw CFMLEngineFactory.getInstance().getExceptionUtil().createFunctionException(pc, "ImageResize", 5, "blurFactor",
+				"argument blurFactor must be between 0 and 10, now is [" + blurFactor + "]", null);
 
-	// MUST interpolation/blur
-	// if(!"highestquality".equals(interpolation) || blurFactor!=1.0)throw new
-	// ExpressionException("argument interpolation and blurFactor are not supported for function
-	// ImageResize");
+		// MUST interpolation/blur
+		// if(!"highestquality".equals(interpolation) || blurFactor!=1.0)throw new
+		// ExpressionException("argument interpolation and blurFactor are not supported for function
+		// ImageResize");
 
-	image.resize(width, height, interpolation, blurFactor);
-	return null;
-    }
+		image.resize(width, height, interpolation, blurFactor);
+		return null;
+	}
 
-    @Override
-    public Object invoke(PageContext pc, Object[] args) throws PageException {
-	if (args.length == 5) return call(pc, args[0], cast.toString(args[1]), cast.toString(args[2]), cast.toString(args[3]), cast.toDoubleValue(args[4]));
-	if (args.length == 4) return call(pc, args[0], cast.toString(args[1]), cast.toString(args[2]), cast.toString(args[3]));
-	if (args.length == 3) return call(pc, args[0], cast.toString(args[1]), cast.toString(args[2]));
-	if (args.length == 2) return call(pc, args[0], cast.toString(args[1]));
-	if (args.length == 1) return call(pc, args[0]);
-	throw exp.createFunctionException(pc, "ImageResize", 1, 5, args.length);
-    }
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		if (args.length == 5) return call(pc, args[0], cast.toString(args[1]), cast.toString(args[2]), cast.toString(args[3]), cast.toDoubleValue(args[4]));
+		if (args.length == 4) return call(pc, args[0], cast.toString(args[1]), cast.toString(args[2]), cast.toString(args[3]));
+		if (args.length == 3) return call(pc, args[0], cast.toString(args[1]), cast.toString(args[2]));
+		if (args.length == 2) return call(pc, args[0], cast.toString(args[1]));
+		if (args.length == 1) return call(pc, args[0]);
+		throw exp.createFunctionException(pc, "ImageResize", 1, 5, args.length);
+	}
 }
